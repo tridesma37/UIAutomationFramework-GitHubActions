e706a7a2d6ad79c1504301bfc3ed7e0adde1b997
Tridesma Essurgani <tridesma.essurgani77@gmail.com>
2026-07-07 07:50:37 +0700

    Run Chrome in headless mode for GitHub Actions

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -5,29 +5,30 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
 
     public static WebDriver driver;
-
     @Before
     public void setup() {
-
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
     }
 
     @After
     public void tearDown() {
-
         if (driver != null) {
             driver.quit();
         }
-
     }
 }
